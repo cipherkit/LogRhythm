0ddafe7e79f32e17310cff8ac382fd905cbe9427
Matt Squire <mhsquire83@gmail.com>
2020-10-05 13:41:35 -0600

    Better Documentation.

--- Readme.md
+++ Readme.md
@@ -1,4 +1,48 @@
-Assumptions
+### Problem statement
+Given a set of JSON objects (in a file is fine), for each unique ID field show the unique IPs associated with it and
+the number of times that IP appeared.  Also, sum the score for that ID.  Make no assumptions about the incoming JSON.
+Example input:
+{"id":"test","score":12,"ip":"1.2.3.4","message":"Hi"}
+{"id":"test","score":5,"ip":"1.2.3.5"}
+{"id":"test","score":17,"ip":"1.2.3.4"}
+{"id":"test2","score":9,"ip":"1.2.3.4"}
+
+### Example output:
+
+    test:
+        1.2.3.5: 1
+        1.2.3.4: 2
+        score: 34
+    test2:
+        1.2.3.4: 1
+        score: 9
+
+### Goal: 
+Design and implement both the program to accomplish this, and the script to test the program.  Any language you
+want to use is fine.  Send back the source code for each, along with any assumptions you made.
+
+### Usage:
+
+        instance = Parser()
+        instance.data_in(data) # where data is a valid JSON string
+        print(instance)
+        instance.clear()
+        ... Do something else with it ...
+
+
+### Desirable Feature List:
+
+* Create json input &#x2611;
+* Clear object &#x2611;
+* Output a str &#x2611;
+* Output an object
+* Create file input
+* Logging
+* Update input
+* String Formatting
+* Command Line Interface
+
+### Assumptions:
 
 1. That input can be a malformed attempt at JSON and should trigger an error.
 3. That Create, Retrieve, Update and Destroy operations should be implemented to some extent.
--- main.py
+++ main.py
@@ -3,23 +3,15 @@ import json
 
 class Parser(object):
     """
-    Given a set of JSON objects (in a file is fine), for each unique ID field show the unique IPs associated with it and
-    the number of times that IP appeared.  Also, sum the score for that ID.  Make no assumptions about the incoming JSON.
-    Example input:
-    {"id":"test","score":12,"ip":"1.2.3.4","message":"Hi"}
-    {"id":"test","score":5,"ip":"1.2.3.5"}
-    {"id":"test","score":17,"ip":"1.2.3.4"}
-    {"id":"test2","score":9,"ip":"1.2.3.4"}
-    Example output:
-    test:
-        1.2.3.5: 1
-        1.2.3.4: 2
-        score: 34
-    test2:
-        1.2.3.4: 1
-        score: 9
-    Goal: Design and implement both the program to accomplish this, and the script to test the program.  Any language you
-    want to use is fine.  Send back the source code for each, along with any assumptions you made.
+        This is a script to simply get the job of parsing input. It's a top down approach for a
+        Non-recursive descent parser. This approach was chosen for it's simplicity to implement.
+
+        Usage:
+        instance = Parser()
+        instance.data_in(data) where data is a valid JSON string
+        print(instance)
+        instance.clear()
+        ... Do something else with it ...
     """
 
     def __init__(self, file_data="{}"):
@@ -34,7 +26,6 @@ class Parser(object):
             return 201, "Object created!"
         except ValueError:
             print(f"Value Error for: {data}")
-
 
     def update(self):
         pass
@@ -79,7 +70,6 @@ class Parser(object):
 
 
 if __name__ == "__main__":
-    # input_file = "example.json.txt"
     parse = Parser()
     parse.data_in('{"id": "test", "score": 12, "ip": "1.2.3.4", "message": "Hi"}')
     parse.data_in('{"id": "test", "score": 5, "ip": "1.2.3.5"}')
